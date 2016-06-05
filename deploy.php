c8c2d14280592085e44bb2cc4a2ef00d624e754e
Bartek Makowski <bartek@advantinteractive.com>
2016-06-05 12:51:53 +0200

    cleared old lines

--- deploy.php
+++ deploy.php
@@ -1,12 +1,9 @@
 <?php
-define('PRIVATE_KEY', 'nL9D7TQWMcVBE5u');
+define('PRIVATE_KEY', 'BLvzt7HRiLrI4xUny1');
 
 if ($_GET['key'] === PRIVATE_KEY)
 {
-    //echo shell_exec("/usr/bin/git pull 2>&1");
     echo shell_exec("/usr/bin/git fetch --all 2>&1");
     echo shell_exec("/usr/bin/git reset --hard origin/development 2>&1");
-//    chdir(realpath(dirname(__FILE__)) . "/wp-content/themes/newadvant/");
-//    echo shell_exec("/usr/bin/gulp &");
 }
-//end of new deployment test
+//end
